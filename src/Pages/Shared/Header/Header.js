ba9976b784b742bd7aa2c9b807df0eed686e5dd8
Abdul-Rakib92 <rakibctgu@gmail.com>
2022-04-18 04:46:11 +0600

    home page is fully responsiv & header component added

--- src/Pages/Shared/Header/Header.js
+++ src/Pages/Shared/Header/Header.js
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import './Header.css';
 
 const Header = () => {
-    return (
-        <header>
-            <h2>This is Header</h2>
-        </header>
-    );
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Muscle Stone <small><span className="web-title">Personal Coach</span></small></Navbar.Brand>
+          <Nav className="ms-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 };
 
 export default Header;
