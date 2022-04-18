3f2a26f17bef7f7ab6b0e79fee8cbf59479232f4
Abdul-Rakib92 <rakibctgu@gmail.com>
2022-04-18 13:39:21 +0600

    checkout & about component compleated

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import rakib from '../../images/rakib.jpg'
+import './About.css';
 
 const About = () => {
     return (
-        <div>
-            <h2>This is about me</h2>
+        <div className='container'>
+            <div className='about-photo'>
+                <img src={rakib} alt="" />
+            </div>
+            <div className='text-container'>
+            <h1>My <span>Vision</span></h1>
+            <p>My name is Abdul Rakib. I am graduate person. My future goal is to become a good web developer. So I practice coding everyday more then 8 hour. I believe with hard work and practice, I can able to feel the interest of coding. My vision is to get a good job after finish my programing course. </p>
+            </div>
         </div>
     );
 };
--- src/Pages/Checkout/Checkout.js
+++ src/Pages/Checkout/Checkout.js
@@ -1,10 +1,23 @@
 import React from 'react';
+import coach from '../../images/coach.jpg';
+import './Checkout.css';
 
 const Checkout = () => {
     return (
-        <div className='container'>
-            <h2>This is Checkout page</h2>
+        <div className="container">
+        <div className="checkout-photo">
+            <img src={coach} alt="" />
         </div>
+        <div className="checkout-text">
+            <h5>a few words about me</h5>
+            <h1>Read My Story</h1>
+            <h4>“I am the creator of a Training Programs, personal coach and a writer. ”</h4>
+            <p>Being fit and living healthy is what I know and love. Fitness has always been a part of my life. I grew up in a fit family—my dad ran marathons, we had a home gym, and my parents always had gym memberships</p>
+            
+            
+        </div>
+
+    </div>
     );
 };
 
